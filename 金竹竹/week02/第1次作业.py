e39c516be29ba1efdf28460e6927d3fbc76f53bc
zhuzhu-j <1312086817@qq.com>
2024-12-18 23:06:51 +0800

    Update 第1次作业.py

--- 金竹竹/week02/第1次作业.py
+++ 金竹竹/week02/第1次作业.py
@@ -1,1 +1,76 @@
+class TorchModel(nn.Module):
+    def __init__(self, input_size):
+        super(TorchModel, self).__init__()
+        self.linear = nn.Linear(input_size, 5)
+        self.activation = nn.Softmax()
+        self.loss = nn.CrossEntropyLoss()
 
+    def forward(self, x, y=None):
+        x = self.linear(x)
+        y_pred = self.activation(x)
+        if y is not None:
+            return self.loss(y_pred, y)
+        else:
+            return y_pred
+
+def build_sample():
+    x = np.random.random(5)
+    max_index = 0
+    max_value = x[0]
+    for i in range(5):
+        if x[i] > max_value:
+            max_index = i
+            max_value = x[i]
+    return max_index
+
+def build_dataset(total_sample_num):
+    X = []
+    Y = []
+    for i in range(total_sample_num):
+        x, y = build_sample()
+        X.append(x)
+        Y.append([y])
+    return torch.FloatTensor(X), torch.FloatTensor(Y)
+
+def evaluate(model):
+    model.eval()
+    test_sample_num = 100
+    x, y = build_dataset(test_sample_num)
+    correct, wrong = 0, 0
+    with torch.no_grad():
+        y_pred = model(x)
+        for y_p, y_t in zip(y_pred, y):
+            class_pred, max_pred =  max((i, v) for i, v in enumerate(y_p))
+            class_ture = int(y_t)
+            if class_pred == class_ture:
+                correct += 1
+            else:
+                wrong += 1
+    print("正确预测个数：%d, 正确率：%f" % (correct, correct / (correct + wrong)))
+    return correct / (correct + wrong)
+
+def main():
+    epoch_num = 20
+    batch_size = 20
+    train_sample = 5000
+    input_size = 5
+    learning_rate = 0.001
+    model = TorchModel(input_size)
+    optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    log = []
+    train_x, train_y = build_dataset(train_sample)
+    for epoch in range(epoch_num):
+        model.train()
+        watch_loss = []
+        for batch_index in range(train_sample // batch_size):
+            x = train_x[batch_index * batch_size : (batch_index + 1) * batch_size]
+            y = train_y[batch_index * batch_size : (batch_index + 1) * batch_size]
+            loss = model(x, y)
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
+            watch_loss.append(loss.item())
+        print("=========\n第%d轮平均loss:%f" % (epoch + 1, np.mean(watch_loss)))
+        acc = evaluate(model)
+        log.append([acc, float(np.mean(watch_loss))])
+    torch.save(model.state_dict(), "model.bin")
