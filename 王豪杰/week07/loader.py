b3f0548974b6343ffc85ecdc11ee3925a86d26db
Wmoil <108467164+Wmoil@users.noreply.github.com>
2025-01-09 18:25:20 +0800

    Update loader.py

--- 王豪杰/week07/loader.py
+++ 王豪杰/week07/loader.py
@@ -7,43 +7,50 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer
+import random
+from config import Config
+import pandas as pd
 """
 数据加载
 """
-
+random.seed(Config["seed"])
 
 class DataGenerator:
-    def __init__(self, data_path, config):
+    def __init__(self, data_path, config, is_train=True):
         self.config = config
         self.path = data_path
-        self.index_to_label = {0: '家居', 1: '房产', 2: '股票', 3: '社会', 4: '文化',
-                               5: '国际', 6: '教育', 7: '军事', 8: '彩票', 9: '旅游',
-                               10: '体育', 11: '科技', 12: '汽车', 13: '健康',
-                               14: '娱乐', 15: '财经', 16: '时尚', 17: '游戏'}
+        self.index_to_label = {0: '差评', 1: '好评'}
         self.label_to_index = dict((y, x) for x, y in self.index_to_label.items())
         self.config["class_num"] = len(self.index_to_label)
         if self.config["model_type"] == "bert":
             self.tokenizer = BertTokenizer.from_pretrained(config["pretrain_model_path"])
         self.vocab = load_vocab(config["vocab_path"])
         self.config["vocab_size"] = len(self.vocab)
-        self.load()
+        self.load(is_train)
 
 
-    def load(self):
+    def load(self, is_train=True):
+        # 读取 csv 文件
         self.data = []
-        with open(self.path, encoding="utf8") as f:
-            for line in f:
-                line = json.loads(line)
-                tag = line["tag"]
-                label = self.label_to_index[tag]
-                title = line["title"]
-                if self.config["model_type"] == "bert":
-                    input_id = self.tokenizer.encode(title, max_length=self.config["max_length"], pad_to_max_length=True)
-                else:
-                    input_id = self.encode_sentence(title)
-                input_id = torch.LongTensor(input_id)
-                label_index = torch.LongTensor([label])
-                self.data.append([input_id, label_index])
+        train_data_rate = self.config["train_data_rate"]#训练集占比
+        csv_data = np.array(pd.read_csv(self.path))
+        random.shuffle(csv_data)
+        train_data = csv_data[:int(train_data_rate * len(csv_data))] #训练数据
+        test_data = csv_data[int(train_data_rate * len(csv_data)):-1] #测试数据
+        print(len(train_data), len(test_data))
+
+        temp_data = train_data if is_train else test_data
+
+        for [label, comment, *_] in temp_data:
+            token = None
+            if self.config["model_type"] == "bert":
+                token = self.tokenizer.encode(comment, max_length=self.config["max_length"], pad_to_max_length=True)
+            else:
+                token = self.encode_sentence(comment)
+            token = torch.LongTensor(token)
+            label = torch.LongTensor([label])
+
+            self.data.append([token, label])
         return
 
     def encode_sentence(self, text):
@@ -75,12 +82,10 @@ def load_vocab(vocab_path):
 
 
 #用torch自带的DataLoader类封装数据
-def load_data(data_path, config, shuffle=True):
-    dg = DataGenerator(data_path, config)
+def load_data(data_path, config, is_train=True, shuffle=True):
+    dg = DataGenerator(data_path, config, is_train)
     dl = DataLoader(dg, batch_size=config["batch_size"], shuffle=shuffle)
     return dl
 
 if __name__ == "__main__":
-    from config import Config
-    dg = DataGenerator("valid_tag_news.json", Config)
-    print(dg[1])
+    dg = DataGenerator("文本分类练习.csv", Config)
