03ebf1b048649371faaed6d0732e42fabcd74b57
Wmoil <108467164+Wmoil@users.noreply.github.com>
2025-01-23 18:51:50 +0800

    Update config.py

--- 王豪杰/week09/config.py
+++ 王豪杰/week09/config.py
@@ -8,7 +8,7 @@ Config = {
     "model_path": r"D:\projects\nlp\week9\model_output",
     "schema_path": r"D:\projects\nlp\week9\ner_data\schema.json",
     "train_data_path": r"D:\projects\nlp\week9\ner_data/train",
-    "valid_data_path": "r"D:\projects\nlp\week9\ner_data/test",
+    "valid_data_path": r"D:\projects\nlp\week9\ner_data/test",
     "vocab_path":r"D:\projects\nlp\week9\chars.txt",
     "max_length": 100,
     "hidden_size": 768,
