c674dddc70cf13ac1c81d6dc6151adc5594d066c
wlsyan233 <31469606+wlsyan233@users.noreply.github.com>
2025-03-26 14:39:44 +0800

    Create config.py

---
+++ 王良顺/week16/config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/3/24 9:40
+# @Author  : WLS
+# @File    : config.py
+# @Software: PyCharm
+"""
+配置参数信息
+"""
+
+Config = {
+    "scenario_path": "./scenario/", # 场景路径
+    "slot_template_path": "./scenario/slot_fitting_templet.xlsx", # 槽位模板路径
+    "repeat_templet_path": "./scenario/repeat_templet.xlsx", # 重复说明模板路径
+    "repeat_node_name": "repeat_node" # 重复节点的名称
+}
